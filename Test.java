8e179747ce2783052f1b5607ea3b6035a5a7029e
hashmmaheaalam <haalam@dminc.com>
2022-04-01 10:31:37 +0530

    How we can call a method

--- Test.java
+++ Test.java
@@ -1,17 +1,14 @@
- //Two type of methods and all methods have 3 parts 1) method declaration 2) method implementation 3) method call 
+ // method calling 
  public class Test {
-	 int a=10,b=30;
-	 //method:- methods are used for writing the logics of application
-	//static method
-	public static void main(String[] args){     //static method memory allocated with class loading so we can access static method via class 
+	public static void main(String[] args){      
 	Test t= new Test();
-	  System.out.println(t.a+t.b +" in static method");
-	  
-	  t.sampleMethod();   //3) method call 
+	 t.sampleMethod();   //instance method memory allocated when object is created so we can access these methods via object only e.g. it get called by 't'
+	 Test.sampleStaticMethod();    //static method memory allocated with class loading so we can access static method via class
 	  }
-	  //instance method 
-	  //1)method declaration
-	void sampleMethod(){   //instance method memory allocated when object is created so we can access these methods via object only e.g. it get called by 't'
-		System.out.println("Hi instance method");   //2) method implementation
+	  void sampleMethod(){   
+	  System.out.println("Hi instance method");   
 	}
+	static void sampleStaticMethod(){
+		System.out.println("Hi static method"); 
+	}		
 }

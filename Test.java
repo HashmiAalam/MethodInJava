3520e2c450917ee3cfed54081b0475a5758856e4
hashmmaheaalam <haalam@dminc.com>
2022-04-01 11:24:29 +0530

    method calling with argument list

--- Test.java
+++ Test.java
@@ -1,14 +1,22 @@
- // method calling 
+ // method calling with arguments
  public class Test {
 	public static void main(String[] args){      
 	Test t= new Test();
-	 t.sampleMethod();   //instance method memory allocated when object is created so we can access these methods via object only e.g. it get called by 't'
-	 Test.sampleStaticMethod();    //static method memory allocated with class loading so we can access static method via class
+	 t.sampleMethod(12,'z',"alam");   //sampleMethod is accepting arguments of type int, char and String
+	 Test.sampleStaticMethod((byte)5,60.5f, 30.4);    //sampleStaticMethod is accepting arguments of type byte, float, double
+	 // if we pass like Test.sampleStaticMethod((5,60.5f); -> it will through error like  incompatible types: possible lossy conversion from int to byte
+	 //The Java language requires you to use a (byte) cast when you assign an int-valued expression to a byte.
 	  }
-	  void sampleMethod(){   
-	  System.out.println("Hi instance method");   
+	  void sampleMethod(int a,char ch,String s){   
+	  System.out.println("Hi instance method with arguments");
+      System.out.println(a);
+      System.out.println(ch);
+      System.out.println(s);	  
 	}
-	static void sampleStaticMethod(){
-		System.out.println("Hi static method"); 
+	static void sampleStaticMethod(byte b, float f,double d){
+		System.out.println("Hi static method with arguments");
+       System.out.println(b);
+       System.out.println(f);
+       System.out.println(d);	   
 	}		
 }

5be75c527fc6589dee381955b723c9b77589f3d2
hashmmaheaalam <haalam@dminc.com>
2022-04-03 18:37:01 +0530

    local variables in java

--- Test.java
+++ Test.java
@@ -1,27 +1,15 @@
- // method calling with arguments in realtime project level
- // In realtime projects methods are Accepecting objects as arguments
- public class Test {
-	public static void main(String[] args){      
-		 //to call sampleInstanceMethod we need object of class Test
-         Test t= new Test();
-         //to call sampleInstanceMethod also we need objects of class Employee and X 
-         Employee employee= new Employee();
-         X x= new X();
-         t.sampleInstanceMethod(employee,x);
-        //to call sampleStaticMethod we do not need object but we need objects of class Student and Y
-        Student student= new Student();
-        Y y= new Y();
-        Test.sampleStaticMethod(student,y);		
+ // method calling with arguments(local variable) 
+  public class Test {
+	 int a=10,b=20;    //instance variable
+	public static void main(String[] args){   
+	//to call add method we need object
+	Test t=new Test();
+	t.add(100,200);    //we need to pass arguments to call add method
 	}
-		void sampleInstanceMethod(Employee e,X x){
-			System.out.println("Hi Employee and x objects");
-		}
-		static void sampleStaticMethod(Student s,Y y){
-			System.out.println("Hi Student and y objects");
-		}
+	void add(int x, int y){     //x and y are called local variable
+	System.out.println(a+b);
+	System.out.println(x+y);
+	}
+	
 }
 
-class Employee{}
-class Student{}
-class X{}
-class Y{}

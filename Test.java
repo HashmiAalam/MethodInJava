aab2bc9a68d6d48ede6acfc8261600652c5f1b48
hashmmaheaalam <haalam@dminc.com>
2022-04-03 18:41:41 +0530

    local variables are having first priority over instance in java

--- Test.java
+++ Test.java
@@ -1,4 +1,5 @@
- // method calling with arguments(local variable) 
+ // method calling with arguments(local variable) v/s instance variable
+//local variables having first priority 
   public class Test {
 	 int a=10,b=20;    //instance variable
 	public static void main(String[] args){   
@@ -6,9 +7,9 @@
 	Test t=new Test();
 	t.add(100,200);    //we need to pass arguments to call add method
 	}
-	void add(int x, int y){     //x and y are called local variable
+	void add(int a, int b){     //a and b are called local variable
+	System.out.println(a+b);   //whenever there is common name in variables local variables get first priority
 	System.out.println(a+b);
-	System.out.println(x+y);
 	}
 	
 }

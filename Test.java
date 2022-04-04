79b6e98dac64ad572253071fdb1a7f75ed378d9b
hashmmaheaalam <haalam@dminc.com>
2022-04-04 17:36:52 +0530

    when we use this keyword with int return value it will return instance one

--- Test.java
+++ Test.java
@@ -1,13 +1,13 @@
-// method return type is interge/number:- it will check for local first then search for instance variable
+// method return type is interge/number:- if we use this keyword than it will search for instance directly
 public class Test {
 	int a=100;
 	public static void main(String[] args){   
 	Test t= new Test();
-	int a=t.sampleMethod();
+	int a=t.sampleMethod(10);
 	System.out.println("method return value= "+a);
 	}
-	int sampleMethod(){
+	int sampleMethod(int a){
 		System.out.println("This is a sampleMethod");
-		return a;
+		return this.a;
 	}
 }

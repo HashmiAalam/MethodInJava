1a13b453b1e65d9e3559ded84efef19f29d1547e
hashmmaheaalam <haalam@dminc.com>
2022-04-04 17:33:23 +0530

    when method returns integer, first priority goes to local variable if it not get local it search for instance

--- Test.java
+++ Test.java
@@ -1,12 +1,12 @@
-// method return type is interge/number:- first priority goes to local variable
+// method return type is interge/number:- it will check for local first then search for instance variable
 public class Test {
 	int a=100;
 	public static void main(String[] args){   
 	Test t= new Test();
-	int a=t.sampleMethod(10);
+	int a=t.sampleMethod();
 	System.out.println("method return value= "+a);
 	}
-	int sampleMethod(int a){
+	int sampleMethod(){
 		System.out.println("This is a sampleMethod");
 		return a;
 	}
